7a1381ba94648212758175831c4fab19bcf2513b
Mati <matiepol9@gmail.com>
2025-10-27 14:42:08 +0100

    poprawka chatbot na jsx składnie wygląd path

--- src/components/ui/Chatbot.tsx
+++ src/components/ui/Chatbot.tsx
@@ -163,7 +163,7 @@ export default function Chatbot() {
 
           {/* GŁÓWNY PRZYCISK Z KOLOREM #8265db */}
           <div 
-            className="relative px-5 py-3 rounded-full backdrop-blur-xl shadow-2xl hover:shadow-[0_8px_32px_rgba(130,101,219,0.4)] transition-all duration-300 hover:scale-105 flex items-center gap-3 hover:cursor-pointer border border-[#8265db]/30 scale-[0.67] md:scale-100"
+            className="relative px-5 py-3 rounded-full backdrop-blur-xl shadow-2xl hover:shadow-[0_8px_32px_rgba(130,101,219,0.4)] transition-all duration-300 hover:scale-105 flex items-center gap-3 hover:cursor-pointer border border-[#8265db]/30 scale-[0.80] md:scale-100"
             style={{
               backgroundColor: '#8265db',
             }}
@@ -176,7 +176,7 @@ export default function Chatbot() {
             ></div>
 
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-white relative z-10 group-hover:scale-110 transition-transform duration-300">
-              <path fill-rule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clip-rule="evenodd" />
+              <path fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clipRule="evenodd" />
             </svg>
 
             
